561471099aef64a490620db9ea004efb704d1e1a
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-02-02 15:35:50 +0300

    finished

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
 ﻿// Программа, которая определяет соответствие закрытых скобок открытым и правильность очередности их написания
 System.Console.WriteLine("Введите в любой последовательности открытые и закрытые скобки");
 string text = Console.ReadLine();
-void DoTheBracketsMatch (string inputText)
+void DoTheBracketsMatch (string inputText, char leftBracket, char rightBracket)
 {
     int quantityLeftBrackets = 0;
     int quantityRightBrackets = 0;
-    bool correctWriting;
+    bool correctWriting = true;
     for (int i = 0; i < text.Length; i++)
     {
-        if (text[i] == '(' || text[i] == ')') System.Console.WriteLine("Вы ввели символы, отличные от скобок");
-        else if (text[i] == '(') quantityLeftBrackets++;
-        else if (text[i] == '(') quantityRightBrackets++;
+        if (text[i] != leftBracket && text[i] != rightBracket) System.Console.WriteLine("Вы ввели символы, отличные от скобок");
+        else if (text[i] == leftBracket) quantityLeftBrackets++;
+        else if (text[i] == rightBracket) quantityRightBrackets++;
         else if (quantityRightBrackets>quantityLeftBrackets)
         {
             correctWriting = false;
@@ -18,5 +18,11 @@ void DoTheBracketsMatch (string inputText)
             break;
         }
     }
+    if (quantityLeftBrackets == quantityRightBrackets) System.Console.WriteLine(correctWriting);
+    else
+    {
+        correctWriting = false;
+        System.Console.WriteLine(correctWriting);
+    }
 }
-DoTheBracketsMatch(text);
+DoTheBracketsMatch(text, '(', ')');
