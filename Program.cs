e7c9aee998e6b6178654adc2dd81f7712c0ee9b1
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-02-02 15:11:48 +0300

    1st try

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая определяет соответствие закрытых скобок открытым и правильность очередности их написания
+System.Console.WriteLine("Введите в любой последовательности открытые и закрытые скобки");
+string text = Console.ReadLine();
+void DoTheBracketsMatch (string inputText)
+{
+    int quantityLeftBrackets = 0;
+    int quantityRightBrackets = 0;
+    bool correctWriting;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] == '(' || text[i] == ')') System.Console.WriteLine("Вы ввели символы, отличные от скобок");
+        else if (text[i] == '(') quantityLeftBrackets++;
+        else if (text[i] == '(') quantityRightBrackets++;
+        else if (quantityRightBrackets>quantityLeftBrackets)
+        {
+            correctWriting = false;
+            System.Console.WriteLine(correctWriting);
+            break;
+        }
+    }
+}
+DoTheBracketsMatch(text);
